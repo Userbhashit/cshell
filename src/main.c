456fee70be55aed1f7e0bc84f13555d0cc1b35cf
Bhashit Gautam <bhashitgtm004@gmail.com>
2026-06-18 22:03:33 +0530

    Add function to tokenize command input

--- src/main.c
+++ src/main.c
@@ -1,22 +1,60 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
 
+#define TOKEN_BUF_SIZE 8
+#define DELIMITERS " \t\n\r\a"
+char** parseQuery(char* query);
+
 int main(void) {
-  
-  char* commandBuffer = NULL;
+  char** command;
+  char* queryBuffer = NULL;
   size_t linecap = 0;
-  ssize_t commandSize;
+  ssize_t bufferSize;
   while (1) {
     printf("chsell> ");
 
-    if ((commandSize = getline(&commandBuffer, &linecap, stdin)) < 0)
+    if ((bufferSize = getline(&queryBuffer, &linecap, stdin)) < 0)
       break;
 
-    commandBuffer[commandSize - 1] = '\0';
-    printf("Searching and executing: %s\n", commandBuffer);
+    queryBuffer[bufferSize - 1] = '\0';
+    command = parseQuery(queryBuffer);
+
+    free(command);
   }
 
-  free(commandBuffer);
+  free(queryBuffer);
 
-  return 0;
+  return EXIT_SUCCESS;
 }
+
+char** parseQuery(char* query) {
+  size_t pos = 0, bufSize = TOKEN_BUF_SIZE;
+  char** tokens =  malloc(bufSize * sizeof(char*));
+  if (!tokens) {
+    fprintf(stderr, "Unable to allocate memory.\n");
+    exit(EXIT_FAILURE);
+  }
+  char* token = strtok(query, DELIMITERS);
+
+  while (token) {
+    tokens[pos++] = token;
+
+    if (pos >= bufSize) {
+      bufSize += TOKEN_BUF_SIZE;
+      tokens = realloc(tokens, bufSize * sizeof (char*));
+
+      if (!tokens) {
+        fprintf(stderr, "Unable to allocate memory.\n");
+        exit(EXIT_FAILURE);
+      } 
+    }
+
+    token = strtok(NULL, DELIMITERS);
+  }
+
+  tokens[pos] = NULL;
+  return tokens;
+}
+
